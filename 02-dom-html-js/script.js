81532ce4d77bde872ac2478ee1537e5573f7a763
CabosManuel <cabos.apolaya@gmail.com>
2022-08-18 10:58:40 -0500

    Reading HTML from JS

--- 02-dom-html-js/script.js
+++ 02-dom-html-js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+//console.log('Hello world!');
+
+// QuerySelector usa selectores CSS
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const paragraph = document.querySelector('.paragraph');
+const pId = document.querySelector('#pId');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+  h1,
+  p,
+  paragraph,
+  pId,
+  input
+});
